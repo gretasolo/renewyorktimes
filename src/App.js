1003c9659b143a52e43950d1250851585f392918
Greta Solomon <gs2806@nyu.edu>
2019-10-15 22:28:41 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="New">
+     <strong><p id="one"> The New York Times</p></strong>
     </div>
+
+    <div className="Content">
+    
+    <p id="twoshift">THE SHIFT
+    Kodak's Dubious Blockchain Gamble
+    What's a 130-year-old photo company doing dabbling in cryptocurrency? Either revolutionizing digital rights management or trying to make a quick buck. 
+    By KEVIN ROOSE</p> 
+   
+   <hr></hr>
+
+    <p id="threecontent">Taiwan Retaliates Against Chinese Airlines, Hampering Lunar New Year Travel
+    By CHRIS HORTON</p>
+
+  <hr></hr>
+
+    <p id="fourcontent">New Jersey Embraces an Idea it Once Rejected: Make Utilities Pay to Emit Carbon
+    By BRAD PLUMMER</p>
+
+    <hr></hr>
+   </div>
+   <div className="Dates">
+   <p id="twodate">Jan.30, 2018</p>
+   <p id="threedate">Jan.30, 2018</p>
+   <p id="fourdate">Jan.29, 2018</p>
+  </div>
+   </div>
   );
 }
 
